6f3699824ac61675d7bb017e4967b45723e573d8
macastil <gmacastil@gmail.com>
2024-01-20 10:39:46 -0500

    ajuste env

--- src/main/java/com/lite/Controller.java
+++ src/main/java/com/lite/Controller.java
@@ -1,14 +1,22 @@
 package com.lite;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	
+	@Value("${env}")
+	private String env;
+	
+	@Value("${host}")
+	private String host;
+	
+	
 	@GetMapping
 	public String get() {
-		return "hola equipo del curso del sabado";
+		return env + ":" + host;
 	}
 
 }
